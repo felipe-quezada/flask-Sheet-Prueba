4068c357d2e8d2a3cd1ca2f54fbeaa056a53259a
Luis <fluis285@gmail.com>
2023-09-28 13:27:30 -0300

    app creation base

--- app/app.py
+++ app/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
-
+from sheet_as_db import sheet
 
 # create serve
 app = Flask(__name__)
 
+# ruta home
 @app.route('/')
 def index():
   return render_template('index.html')
+
+# envia toda la hoja de calculo
+@app.route('/list')
+def get_list():
+  get_list = sheet.get_all_records()
+  return get_list
 
 if (__name__ == '__main__'):
   app.run(debug=True)
